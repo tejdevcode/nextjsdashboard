735fa06236edac1c5ed1c288085dc90a32bbd1e5
tejrdev <tejashkumar.r@uplers.in>
2024-12-29 14:16:21 +0530

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+
+
+const Page = () => {
+  return (
+    <div><p>Customers Page</p></div>
+  )
+}
+
+export default Page
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+
+
+const Page = () => {
+   return (
+     <div><p>Invoices Page</p></div>
+   )
+ }
+ 
+ export default Page
