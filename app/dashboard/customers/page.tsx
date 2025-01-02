855c3974abbf2c193f0ce3e64e383b0fe0b18271
tejrdev <tejashkumar.r@uplers.in>
2025-01-02 11:32:27 +0530

    auth done

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,8 +1,27 @@
 
+import Table from '@/app/ui/customers/table';
+import Pagination from '@/app/ui/invoices/pagination';
+import { TableRowSkeleton } from '@/app/ui/skeletons';
+import { fetchCustomers, fetchFilteredCustomers } from '@/app/lib/data';
+import { Suspense } from 'react';
 
-const Page = () => {
+
+const Page = async (props: {
+  searchParams?: Promise<{ query?: string; page?: string; }>; }) => {
+     const searchParams = await props.searchParams;
+     const query = searchParams?.query || '';
+     const currentPage = Number(searchParams?.page) || 1;
+     const customers = await fetchFilteredCustomers(query);
+     /* const customers = await fetchCustomers(); */
   return (
-    <div><p>Customers Page</p></div>
+    <div className="w-full">      
+       <Suspense key={query + currentPage} fallback={<TableRowSkeleton />}>
+        <Table  customers={customers} />
+      </Suspense>
+      {/* <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div> */}
+    </div>
   )
 }
 
